7e7095a3cc60331ab0c0bc955212009905b84524
flesnuk <flesnuk@gmail.com>
2018-04-16 23:12:48 +0200

    ditch powershell and use syscalls

--- windowtitle.go
+++ windowtitle.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"strings"
+	"github.com/mitchellh/go-ps"
 )
 
-// credits to @txanatan https://github.com/txanatan/ppwatch/blob/master/main.go
-
-const getWindowTitlePowershellExpression = `gps |
-? {$_.mainwindowtitle -like "*%s*"} |
-? {$_.processname -like "%s"} |
-select mainwindowtitle`
-
 func isOsuOpen() bool {
-	PartialWindowTitle := "osu!"
-	ProcessName := "osu!"
-	expr := fmt.Sprintf(getWindowTitlePowershellExpression, PartialWindowTitle, ProcessName)
-	cmd := exec.Command("powershell", "-command", expr)
+	processName := "osu!.exe"
+	processes, err := ps.Processes()
 
-	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return false
 	}
 
-	// Strip all leading/trailing whitespace, separate into lines, delete empty lines
-	var outlines []string
-	for _, s := range strings.Split(strings.Trim(string(out), " "), "\r\n") {
-		if s != "" {
-			outlines = append(outlines, s)
+	for _, p := range processes {
+		if p.Executable() == processName {
+			return true
 		}
 	}
-
-	// Check that we have content in the array, if not the window doesn't exist
-	if len(outlines) < 1 {
-		return false
-	}
-	return true
+	return false
 }
